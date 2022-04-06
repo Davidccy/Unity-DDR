f4a91b27aada0203ddbab888dda3ce96793cf85f
David-Win <davidccy0416@gmail.com>
2022-04-06 15:05:10 +0900

    Show random 'Pokemon' in loading UI
    UI
    - Add animation of Pichu and Raichu

--- DDR/Assets/Scripts/UI/Windows/UIWindowLoading.cs
+++ DDR/Assets/Scripts/UI/Windows/UIWindowLoading.cs
@@ -5,10 +5,24 @@ public class UIWindowLoading : MonoBehaviour {
     #region Serialized Fields
     [SerializeField] private CustomPlayableDirector _cpdFadeIn = null;
     [SerializeField] private CustomPlayableDirector _cpdFadeOut = null;
+
+    [SerializeField] private GameObject[] _goPokemonArray = null;
+    #endregion
+
+    #region Internal Fields
+    private int _pokemonIndex = 0;
+    #endregion
+
+    #region Mono Behaviour Hooks
+    private void Awake() {
+        HideAllPokemon();
+    }
     #endregion
 
     #region APIs
     public async Task PlayFadeIn() {
+        ShowRandomPokemon();
+
         _cpdFadeOut.Stop();
 
         await _cpdFadeIn.Play();
@@ -18,6 +32,21 @@ public class UIWindowLoading : MonoBehaviour {
         _cpdFadeIn.Stop();
 
         await _cpdFadeOut.Play();
+    }
+    #endregion
+
+    #region Internal Methods
+    private void HideAllPokemon() {
+        for (int i = 0; i < _goPokemonArray.Length; i++) {
+            _goPokemonArray[i].SetActive(false);
+        }
+    }
+
+    private void ShowRandomPokemon() {
+        _goPokemonArray[_pokemonIndex].SetActive(false);
+
+        _pokemonIndex = Random.Range(0, _goPokemonArray.Length);
+        _goPokemonArray[_pokemonIndex].SetActive(true);
     }
     #endregion
 }
