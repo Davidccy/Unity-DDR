16fac494c2a437ed9d012647c9de2c46a6cee240
David-Win <davidccy0416@gmail.com>
2022-04-24 03:22:49 +0900

    Add GameDataManager for saving options and high score
    System
    - Game data manager
    
    Misc
    - File path changing

---
+++ DDR/Assets/Scripts/Data/GameDataManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public static class GameDataManager {
+    #region APIs
+    public static void SaveInt(string key, int value) {
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    public static int LoadInt(string key, int defaultValue = 0) {
+        return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetInt(key) : defaultValue;
+    }
+
+    public static void SaveFloat(string key, float value) {
+        PlayerPrefs.SetFloat(key, value);
+    }
+
+    public static float LoadFloat(string key, float defaultValue = 0) {
+        return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetFloat(key) : defaultValue;
+    }
+
+    public static void SaveString(string key, string value) {
+        PlayerPrefs.SetString(key, value);
+    }
+
+    public static string LoadString(string key, string defaultValue = "") {
+        return PlayerPrefs.HasKey(key) ? PlayerPrefs.GetString(key) : defaultValue;
+    }
+    #endregion
+}
